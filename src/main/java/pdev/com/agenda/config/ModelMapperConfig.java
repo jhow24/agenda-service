4ab58baf64aeecd21d04a456faf68bf0535c0253
Johnathan Rodrigues <deadjhow@gmail.com>
2022-08-18 20:55:13 -0300

    atualização

---
+++ src/main/java/pdev/com/agenda/config/ModelMapperConfig.java
@@ -0,0 +1,14 @@
+package pdev.com.agenda.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modalMapper(){
+        return new ModelMapper();
+    }
+}
